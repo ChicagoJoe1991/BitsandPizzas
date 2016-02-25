c4f8a988bd89d8839c9fa8bd1617e57a419b41fe
ChicagoJoe1991 <dabears1.1.1@gmail.com>
2016-02-24 19:32:01 -0600

    deleted content_main.xml; don't need this when you are generating the layout with java code

--- app/src/main/java/com/example/walter/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/example/walter/bitsandpizzas/OrderActivity.java
@@ -2,6 +2,8 @@ package com.example.walter.bitsandpizzas;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class OrderActivity extends Activity {
 
