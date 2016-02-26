852679bb71b009213bae78efe329a3953f505f0e
ChicagoJoe1991 <dabears1.1.1@gmail.com>
2016-02-26 10:56:46 -0600

    create shareactionprovider

--- app/src/main/java/com/example/walter/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/example/walter/bitsandpizzas/MainActivity.java
@@ -6,8 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.widget.ShareActionProvider;
 
 public class MainActivity extends Activity {
+    private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +22,17 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.share_action);
+        shareActionProvider = (ShareActionProvider) menuItem.getActionProvider();
+        setIntent("This is example text.");
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setIntent(String text){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
     }
 
     @Override
